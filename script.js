d5dbefeca14fb5c7512dbe2ffd52d7e4df433773
farukhraj <faruk.hosen@gmail.com>
2025-09-20 18:42:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/fireba
 import { getFirestore, collection, getDocs, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
 // Firebase config
-const firebaseConfig = { /* your config */ };
+const firebaseConfig = {
+  apiKey: "AIzaSyCC26AlX5xVRo_s0nDI1Ua26JbWh2d1FKk",
+  authDomain: "leads-to-funnel.firebaseapp.com",
+  projectId: "leads-to-funnel",
+  storageBucket: "leads-to-funnel.firebasestorage.app",
+  messagingSenderId: "574308221054",
+  appId: "1:574308221054:web:9a27e9b1e8e8c0270d982f",
+  measurementId: "G-2M5P17GZZJ"
+};
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
@@ -14,14 +23,14 @@ async function loadClients() {
   querySnapshot.forEach(docSnap => {
     const client = docSnap.data();
     const phones = client.phones?.join(", ") || "N/A";
-    const servicesText = client.interestedServices?.map(s => `${s.primary} (${s.sub}) $${s.estimatedPrice}`).join("<br>") || "";
+    const services = client.interestedServices?.map(s => `${s.primary} (${s.sub}) $${s.estimatedPrice}`).join("<br>") || "";
     tableBody.innerHTML += `
       <tr>
         <td>${client.name}</td>
-        <td>${client.stage}</td>
+        <td>${client.stage || ""}</td>
         <td>${phones}</td>
-        <td>${servicesText}</td>
-        <td><button onclick="viewInteractions('${docSnap.id}')">View Interactions</button></td>
+        <td>${services}</td>
+        <td><button onclick="viewInteractions('${docSnap.id}','${client.name}')">View Interactions</button></td>
       </tr>
     `;
   });
@@ -32,7 +41,7 @@ document.getElementById("addClientForm").addEventListener("submit", async e => {
   e.preventDefault();
   await addDoc(collection(db, "clients"), {
     name: document.getElementById("clientName").value,
-    phones: [document.getElementById("clientPhone").value],
+    phones: [document.getElementById("clientPhone1").value, document.getElementById("clientPhone2").value].filter(Boolean),
     stage: document.getElementById("clientStage").value,
     interestedServices: [{
       primary: document.getElementById("clientCountry").value,
@@ -42,14 +51,14 @@ document.getElementById("addClientForm").addEventListener("submit", async e => {
     createdAt: serverTimestamp(),
     updatedAt: serverTimestamp()
   });
-  alert("Client added!");
   e.target.reset();
+  alert("Client added!");
   loadClients();
 });
 
-// Open interactions in new page
-window.viewInteractions = function(clientId) {
-  window.location.href = `interactions.html?clientId=${clientId}`;
+// Open Interactions in new page
+window.viewInteractions = (clientId, clientName) => {
+  window.location.href = `interactions.html?clientId=${clientId}&clientName=${encodeURIComponent(clientName)}`;
 }
 
 // Initial load
